ea87cb75558f79abc79bcbf507cc4ddd1d79cd89
woaichunchunma <woaichunchunma2009@163.com>
2016-07-06 16:11:26 +0800

    Initial Commit

---
+++ Lock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lock
+//
+//  Created by apple on 16/7/6.
+//  Copyright © 2016年 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lock
+//
+//  Created by apple on 16/7/6.
+//  Copyright © 2016年 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
